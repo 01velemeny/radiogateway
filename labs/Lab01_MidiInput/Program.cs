3e24d4e764fd02b7b916e5695d87085e2ee75e70
Patau <308906110+01velemeny@users.noreply.github.com>
2026-07-28 00:04:36 +0200

    feet: first C# project started

---
+++ labs/Lab01_MidiInput/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab01_MidiInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
