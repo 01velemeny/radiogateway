ca8c9100262fe461f6f012e93c01cee3285b4ca1
Patau <308906110+01velemeny@users.noreply.github.com>
2026-07-28 15:30:58 +0200

    Lab02: Initial MIDI device discovery

---
+++ labs/Lab02_MidiDiscovery/Program.cs
@@ -0,0 +1,17 @@
+﻿using Melanchall.DryWetMidi.Multimedia;
+namespace Lab02_MidiDiscovery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== MIDI Input Devices ===");
+            Console.WriteLine();
+            foreach (var inputDevice in InputDevice.GetAll())
+            {
+                Console.WriteLine(inputDevice.Name);
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
