4ef8c27c236033e20ebf5b53e0dfcd2e0dd3ba04
Patau <308906110+01velemeny@users.noreply.github.com>
2026-08-13 00:57:30 +0200

    Lab06_RemoteControl starting project

---
+++ labs/Lab06_RemoteClient/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab06_RemoteClient;
+
+using System.Net.Sockets;
+using System.Text;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        using TcpClient client = new TcpClient();
+        await client.ConnectAsync("127.0.0.1", 5000);
+        Console.WriteLine("Connected to server.");
+
+        using NetworkStream stream = client.GetStream();
+        byte[] message = Encoding.UTF8.GetBytes("PING\n");
+        await stream.WriteAsync(message);
+        Console.WriteLine("Sent: PING");
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = await stream.ReadAsync(buffer);
+        string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        Console.WriteLine($"Received: {response.Trim()}");
+    }
+}
+
---
+++ labs/Lab06_RemoteControl/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Lab06_RemoteControl;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var listener = new TcpListener(IPAddress.Any, 5000);
+        listener.Start();
+        Console.WriteLine("TCP server startded.");
+        Console.WriteLine("Waiting for connection...");
+        while (true)
+        {
+            using TcpClient client = await listener.AcceptTcpClientAsync();
+            Console.WriteLine("Client connected.");
+
+            using NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead = await stream.ReadAsync(buffer);
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Received : {message}");
+
+            if (message.Trim() == "PING")
+            {
+                byte[] response = Encoding.UTF8.GetBytes("PONG\n");
+
+                await stream.WriteAsync(response);
+                Console.WriteLine("Sent: PONG");
+            }
+            Console.WriteLine("Client disconnected.");
+            Console.WriteLine("Waiting for connection...");
+        }
+    }
+}
